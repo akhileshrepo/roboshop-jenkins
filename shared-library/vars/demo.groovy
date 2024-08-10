73fdea236aee43b04c31748a5574b967c3e044ea
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-10 11:36:08 +0530

    updated code

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -5,3 +5,41 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Compile Code') {
+                steps {
+                    echo 'Hello World'
+                    script {
+                        info 'Starting'
+                        warning 'Nothing to do!'
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+            stage('Build') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+            stage('Code Quality') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+            stage('Code Security') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+        }
+    }
+}
