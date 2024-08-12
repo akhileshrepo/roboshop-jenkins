ccb00a4a5298b9d7a4b4f5f5bbff11c44c17cdbf
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-12 17:46:42 +0530

    changes

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,9 +2,16 @@ def call() {
 
     node('workstation') {
 
-        sh "find . | sed -e '1d' | xargs rm -rf "
-        sh 'env'
-        git branch: 'main', url: "https://github.com/akhileshrepo/${component}"
+        sh "find . | sed -e '1d' |xargs rm -rf"
+        if(env.TAG_NAME ==~ ".*") {
+            env.branch_name = "refs/tags/${env.TAG_NAME}"
+        } else {
+            env.branch_name = "${env.BRANCH_NAME}"
+        }
+        checkout scmGit(
+                branches: [[name: branch_name]],
+                userRemoteConfigs: [[url: "https://github.com/raghudevopsb74/${component}"]]
+        )
 
         stage('Compile Code') {
            common.compile()
