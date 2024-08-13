09cc22f5c5986c26cd5ffb4b39b84800c2f4b728
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-14 00:11:36 +0530

    changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -35,8 +35,8 @@ def test() {
 
 def codeQuality() {
     stage('Code Quality') {
-        env.sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube_user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-        env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube_pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
             if(env.codeType == "maven") {
                 //sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
